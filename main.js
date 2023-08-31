6cee20c967e716a3c98e2ab30b1832f94ca7b247
Afong <t225310917@gmail.com>
2023-08-31 21:49:07 +0800

    index update style、logo/ title icon add

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './assets/scss/all.scss';
 import './node_modules/bootstrap/dist/js/bootstrap';
-
-import './assets/js/login-switch';
+import './js/classification-tag';
+import './js/login-switch';
 
 console.log("Hello world!");
