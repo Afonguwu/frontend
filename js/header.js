1d583fef9b2a822e6604ef6de47256ea7a332268
Afong <t225310917@gmail.com>
2023-09-06 11:58:25 +0800

    heaser/footer更新

--- js/header.js
+++ js/header.js
@@ -9,17 +9,34 @@ loginButton.addEventListener('click', () => {
 });
 
 // 前往search頁面
-// seach input text
-const goSearchText = document.querySelector('#goSearchText');
-// 搜索按钮
-const goSearchBtn = document.querySelector('#goSearchBtn');
-// search按鈕
-goSearchBtn.addEventListener('click', event => {
-  event.preventDefault();
-  window.location.href = "search.html";
-});
-//enter輸入
-goSearchText.addEventListener('keydown', event => {
-  event.preventDefault(); // 阻止表单默认提交行为
-  window.location.href = "search.html";
-});
+  // 平板以上
+  // seach input text
+  const goSearchText = document.querySelector('#goSearchText');
+  // 搜索按钮
+  const goSearchBtn = document.querySelector('#goSearchBtn');
+  // search按鈕
+  goSearchBtn.addEventListener('click', event => {
+    event.preventDefault();
+    window.location.href = "search.html";
+  });
+  //enter輸入
+  goSearchText.addEventListener('keydown', event => {
+    event.preventDefault(); // 阻止表单默认提交行为
+    window.location.href = "search.html";
+  });
+
+  // 手機板
+   // seach input text
+  const goSearchFillText = document.querySelector('#goSearchFillText');
+  // 搜索按钮
+  const goSearchFillBtn = document.querySelector('#goSearchFillBtn');
+  // search按鈕
+  goSearchFillBtn.addEventListener('click', event => {
+    event.preventDefault();
+    window.location.href = "search.html";
+  });
+  //enter輸入
+  goSearchFillTex.addEventListener('keydown', event => {
+    event.preventDefault(); // 阻止表单默认提交行为
+    window.location.href = "search.html";
+  });
