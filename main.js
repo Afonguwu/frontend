626d80ef0692f9a12f161ef5795686fdd4e40dca
I401 <milk7456852@gmail.com>
2023-09-07 18:30:43 +0800

    頁面更新-profile & faq

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import './assets/scss/all.scss';
-import './node_modules/bootstrap/dist/js/bootstrap';
+import './node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './js/classification-tag';
 import './js/header';
 
